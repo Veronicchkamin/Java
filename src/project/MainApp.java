cc8f35836689621b58b5d5cf257de20159b673b0
Veronicchkamin <veronichka.minchonak@gmail.com>
2024-12-03 19:08:05 +0100

    commit

---
+++ src/project/MainApp.java
@@ -0,0 +1,20 @@
+package project;
+
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+         System.out.println("enter number");
+        Scanner sc = new Scanner(System.in);
+        var num = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < num; i++ ) {
+            sum = sum +i;
+            System.out.println("sum: " + sum);
+        }
+
+        System.out.println("close");
+    }
+
+}
